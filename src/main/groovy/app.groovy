bbf5bd8db9d4640f339d333a90ec9305292d0197
jocelin79 <jocelinnunes@gmail.com>
2022-10-14 20:10:22 -0300

    update

--- src/main/groovy/app.groovy
+++ src/main/groovy/app.groovy
@@ -1,7 +1,9 @@
 class app {
-    public static void main(String[] args){
+    static int controleDasAbas(String entrada){
 
-        String entrada = "3,5,F,F,C,C"
+        //String entrada = "3,5,F,F,C,C,C"
+        //int saida = 2
+
         int inicial_abas, acoes_pericles;
 
         for(int i=0;i< entrada.length();i++)
