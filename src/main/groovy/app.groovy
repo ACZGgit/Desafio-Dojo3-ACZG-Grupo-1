232148daafbe15ba4982479f222166ac37a175f6
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2022-10-14 20:43:47 -0300

    2 Round Gabriel e Gustavo

--- src/main/groovy/app.groovy
+++ src/main/groovy/app.groovy
@@ -1,26 +1,9 @@
+import parser.Parser
+
 class app {
-    static int controleDasAbas(String entrada){
 
-        //String entrada = "3,5,F,F,C,C,C"
-        //int saida = 2
-
-        int inicial_abas, acoes_pericles;
-
-        for(int i=0;i< entrada.length();i++)
-        {
-            if(entrada[i]!=',')
-            {
-
-            } else if {
-                entrada
-            }
-            break
-
-            if {
-            inicial_abas
-        }
-        }
-        }
-
+    static void main(String[] args) {
+        Parser parser = new Parser()
+        parser.executar('');
     }
-}
+    }
--- src/main/groovy/parser/Parser.groovy
+++ src/main/groovy/parser/Parser.groovy
@@ -5,8 +5,24 @@ class Parser {
 	String executar(String entrada) {
 		String saida = '1'
 
-		//seu codigo aqui
+		//Seu codigo aqui
+		//String entrada = "3,5,F,F,C,C,C"
+
+			int inicial_abas, acoes_pericles;
+			def acoes = []
+
+			for(int i=0;i< entrada.length();i++)
+			{
+				if(entrada[i]!=',')
+				{
+					if(entrada[i+1]!=',')
+					{
+
+					}
+				}
+			}
 
 		return saida
+
 	}
 }
